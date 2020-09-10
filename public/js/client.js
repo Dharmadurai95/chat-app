63cd6a90a9e3282ee584762d5a117c78cf46e637
unknown <dharmamech3695@gmail.com>
2020-09-10 18:47:25 +0530

    error msg show properly

--- public/js/client.js
+++ public/js/client.js
@@ -92,7 +92,7 @@ locationSend.addEventListener('click', () => {
 socket.emit('join', { username, room }, (error) => {
 
     if (error) {
-        alert('error');
+        alert(error);
         location.href = '/'
     }
 })
